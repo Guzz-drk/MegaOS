705b061d9be89371c870d91e0ebc7b9796ab7312
Guzz-drk <182740@upf.br>
2021-04-14 22:34:42 -0300

    Teste Commit -Gustavo

---
+++ Index-LAPTOP-A9RJSO8C.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- introdução de bibliotecas css-->
+        
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="resources/nav.css">
+        <link rel="stylesheet" href="resources/smartmenus/css/sm-core-css.css">
+        <link rel="stylesheet" href="resources/smartmenus/css/sm-mint/sm-mint.css">
+        <!-- introdução de bibliotecas JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script  src = "/resources/smartmenus/jquery.smartmenus.min.js"> </script>
+        
+    </head>
+    <body>
+    <nav class="main-nav" role="navigation">
+
+  <!-- Mobile menu toggle button (hamburger/x icon) -->
+  <input id="main-menu-state" type="checkbox" />
+  <label class="main-menu-btn" for="main-menu-state">
+    <span class="main-menu-btn-icon"></span> Toggle main menu visibility
+  </label>
+
+  <h2 class="nav-brand"><a href="#">MegaOS</a></h2>
+
+  <!-- Sample menu definition -->
+  <ul id="main-menu" class="sm sm-mint">
+    <li><a href="#">Página Inicial</a></li>
+    <li><a href="#">Usuários</a>
+      <ul>
+        <li><a href="#">Incluir Usuários</a></li>
+        <li><a href="#">Listar usuários</a></li>
+        <li><a href="http://vadikom.com/about/#vasil-dinkov"></a></li>
+        <li><a href="http://www.smartmenus.org/about/vadikom/">The company</a>
+          <ul>
+            <li><a href="http://vadikom.com/about/">About Vadikom</a></li>
+            <li><a href="http://vadikom.com/projects/">Projects</a></li>
+            <li><a href="http://vadikom.com/services/">Services</a></li>
+            <li><a href="http://www.smartmenus.org/about/vadikom/privacy-policy/">Privacy policy</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li><a href="http://www.smartmenus.org/download/">Chamados</a></li>
+    <li><a href="http://www.smartmenus.org/support/">Support</a>
+      <ul>
+        <li><a href="http://www.smartmenus.org/support/premium-support/">Premium support</a></li>
+        <li><a href="http://www.smartmenus.org/support/forums/">Forums</a></li>
+      </ul>
+    </li>
+    <li><a href="http://www.smartmenus.org/docs/">Docs</a></li>
+    <li><a href="#">Sub test</a>
+      <ul>
+        <li><a href="#">Dummy item</a></li>
+        <li><a href="#">Dummy item</a></li>
+        <li><a href="#" class="disabled">Disabled menu item</a></li>
+        <li><a href="#">Dummy item</a></li>
+        <li><a href="#">more...</a>
+          <ul>
+            <li><a href="#">A pretty long text to test the default subMenusMaxWidth:20em setting for the sub menus</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">more...</a>
+              <ul>
+                <li><a href="#">Dummy item</a></li>
+                <li><a href="#" class="current">A 'current' class item</a></li>
+                <li><a href="#">Dummy item</a></li>
+                <li><a href="#">more...</a>
+                  <ul>
+                    <li><a href="#">subMenusMinWidth</a></li>
+                    <li><a href="#">10em</a></li>
+                    <li><a href="#">forced.</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Dummy item</a></li>
+                <li><a href="#">Dummy item</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">A pretty long text to test the default subMenusMaxWidth:20em setting for the sub menus</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">A pretty long text to test the default subMenusMaxWidth:20em setting for the sub menus</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">A pretty long text to test the default subMenusMaxWidth:20em setting for the sub menus</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">A pretty long text to test the default subMenusMaxWidth:20em setting for the sub menus</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">A pretty long text to test the default subMenusMaxWidth:20em setting for the sub menus</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+            <li><a href="#">Dummy item</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li><a href="http://www.smartmenus.org/contact/">Contact</a></li>
+  </ul>
+</nav>
+
+<div class="demo-text">
+  <p>Demonstrates a complete navbar including a mobile view menu toggle button (<a href="http://www.smartmenus.org/docs/#menu-toggle-button">related docs</a>).</p>
+</div>
+    </body>
+    <script>
+        
+    $(function() {
+  $('#main-menu').smartmenus({
+    subMenusSubOffsetX: 6,
+    subMenusSubOffsetY: -8
+  });
+});
+
+// SmartMenus mobile menu toggle button
+$(function() {
+  var $mainMenuState = $('#main-menu-state');
+  if ($mainMenuState.length) {
+    // animate mobile menu
+    $mainMenuState.change(function(e) {
+      var $menu = $('#main-menu');
+      if (this.checked) {
+        $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
+      } else {
+        $menu.show().slideUp(250, function() { $menu.css('display', ''); });
+      }
+    });
+    // hide mobile menu beforeunload
+    $(window).bind('beforeunload unload', function() {
+      if ($mainMenuState[0].checked) {
+        $mainMenuState[0].click();
+      }
+    });
+  }
+});
+</script>
+
+</html>
---
+++ View/FormChamado.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--inclusão css-->
+    <link rel="stylesheet" href="/resources/CSS/formchamado.css">
+</head>
+<body>
+ <object data="../menu.php" width="100%"></object>
+<h1>Cadastro de ordem de serviço</h1>
+<div name="div-form-chamado" class="container"> <br>
+<input type="text" placeholder="Nome de usuário:"> <br> <br>
+<input type="text" placeholder="Tipo de equipamento:"> <br> <br>
+<input type="text" placeholder="Marca do Equipamento:"> <br> <br>
+<input type="text" placeholder="Modelo do Equipamento:"> <br> <br>
+<input type="text" placeholder="Cor do Equipamento:">    <br> <br> <br> <br>
+<label for="viciopercebido">--------------------------vicio percebido:-------------------------</label>
+<textarea name="vicio percebido" id="" cols="50" rows="10"></textarea> <br> <br> 
+<input type="button" value="cadastrar Ordem de Serviço">
+
+
+
+
+
+
+</div>
+<br><br><br> <span></span>
+    
+</body>
+</html>
